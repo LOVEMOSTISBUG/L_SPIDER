6bef8f850e7473663fa992ea3a4648c19a25f74f
LOVEMOSTISBUG <56868054+LOVEMOSTISBUG@users.noreply.github.com>
2021-01-19 15:08:55 +0800

    为了配合多线程 多写了几个内置函数

--- L_SPIDER.py
+++ L_SPIDER.py
@@ -16,6 +16,7 @@ class SPIDER():
         self.aim_list = []
         self.deep_list = []
         self.non_bmp_map = dict.fromkeys(range(0x10000,sys.maxunicode + 1),0xfffd)#解决'UCS-2' codec can't encode characters
+        
     def url_open(self,url):
         '''伪造报头和代理IP访问URL并返回值(默认二进制)'''
         ip_list = list(set(open('ip.txt','r').read().split('\n')))
@@ -40,20 +41,7 @@ class SPIDER():
             if str(e.code)[0]=='5':
                 print(e,'\n服务器炸了！哇靠！')
             return ''
-        
-    def get_html_gzip(self):
-        """访问本身地址并用gzip解压后返回且保存URL"""
-        try:
-            result = self.url_open(self.aim_url)
-            buf = StringIO(result)
-            f = gzip.GzipFile(fileobj=buf)
-            html =  f.read()
-            self.html = html
-            print('用gzip解压得到HTML')
-            return self.html
-        except Exception as e:
-            print('gzip解压缩获取HTML也失败了。原因如下：',e)
-                
+                  
     def get_html(self):
         """访问本身地址并返回且保存URL"""
         if self.html=='':
@@ -67,17 +55,26 @@ class SPIDER():
                     self.html = ''
                 return self.html
             except Exception as e:
-                print('获取HTML失败了。失败原因如下：',str(e))
-                return self.get_html_gzip()
-                
+                try:
+                    result = self.url_open(self.aim_url)
+                    buf = StringIO(result)
+                    f = gzip.GzipFile(fileobj=buf)
+                    html =  f.read()
+                    self.html = html
+                    print('用gzip解压得到HTML')
+                    return self.html
+                except Exception as e:
+                    print('gzip解压缩获取HTML也失败了。原因如下：',e)   
         else:
             return self.html
+        
     def show_html(self):
         """输出本身地址HTML"""
         if self.html!='':
             print(self.html)
         else:
             print('请先运行get_html函数。')
+            
     def get_aim_list(self):
         """访问本身地址并返回且保存目标URL列表"""
         try:
@@ -98,10 +95,37 @@ class SPIDER():
                 self.L_print(url)  
         else :
             print('居然不是列表？')
+            
     def crawl(self):
         '''初始化基本的HTML和目标URL列表'''
         self.html = self.get_html()
         self.aim_list = self.get_aim_list()
+
+    def L_print(self,content,warning=False):
+        '''打印出来偶尔会编码错误时用到的print'''
+        s = ''
+        try:
+            print(content)
+        except UnicodeEncodeError as e:
+            s = e
+            print('打印编码错误')
+        if s!= '':
+            if warning:
+                print('其中的乱码原因是这个:',s)
+                
+    def download(self,url,aim_dir=''):
+        '''下载这个东西'''
+        print('目前下载文件URL：'+url)
+        try:
+            file_name = url.split('/')[-1]
+            print('保存文件名为'+file_name)
+            response = self.url_open(url)
+            with open (aim_dir + file_name,'wb') as file:
+                file.write(response)
+        except Exception as e:
+            print('下载出现问题。问题原因如下：',e)
+
+
         
     def download_aim_list(self,aim_dir='',aim_list=[]):
         """按查找到的aim_list逐个下载"""
@@ -121,7 +145,19 @@ class SPIDER():
             except Exception as e:
                 aim_list.append(url)
                 print('下载出现问题。问题原因如下：',e)
-    
+
+    def keep_data_one_page(self,url,separator=','):
+        self.html = self.url_open(self.aim_url).decode('utf-8')
+        self.get_aim_list()
+        with open('data.txt','a+',encoding='utf-8')as f:
+            for i in self.aim_list:
+                if type(i) == str:
+                    f.write(i.lstrip()+'\n')
+                    self.L_print('写入数据：'+ i.lstrip())      
+                elif type(i) == tuple:
+                    f.write(separator.join(list(i))+'\n')
+                    self.L_print('写入数据：'+ separator.join(list(i)))
+s
     def keep_data_by_pages(self,page=1,f_url='',b_url='',separator=',',page_wd=1):
         '''分页爬取并保存数据'''
         for n in range(1,page+1):
@@ -132,18 +168,13 @@ class SPIDER():
             with open('data.txt','a+',encoding='utf-8')as f:
                 for i in self.aim_list:
                     if type(i) == str:
-                        try:
-                            f.write(i.lstrip()+'\n')
-                            print('写入数据：',i.lstrip())
-                        except Exception as e:
-                            f.write(i.lstrip()+'\n')
-                            print('没法打印出来但是已经格式写入了~',e)
+                        f.write(i.lstrip()+'\n')
+                        self.L_print('写入数据：'+ i.lstrip())      
                     elif type(i) == tuple:
                         f.write(separator.join(list(i))+'\n')
-                        try:
-                            print('写入数据：'+separator.join(list(i)))
-                        except Exception as e:
-                            print('元组写入嘛。。我建议你还是重新去看正则吧',e)
+                        self.L_print('写入数据：'+ separator.join(list(i)))
+            print(f'第{n}页搞定！')
+                            
     def deep_crawl(self,k_deep,f_url='',b_url=''):
         '''深入挖掘数据'''
         for url in self.aim_list:
@@ -171,20 +202,6 @@ class SPIDER():
             self.deep_list.extend(ls)
         return self.deep_list    
     
-    def L_print(self,content,warning=False):
-        '''打印出来偶尔会编码错误时用到的print'''
-        s = ''
-        try:
-            print(content)
-        except UnicodeEncodeError as e:
-            s = e
-            print('打印编码错误')
-            #print(str(content).translate(self.non_bmp_map))#解决'UCS-2' codec can't encode characters
-        if s!= '':
-            if warning:
-                print('其中的乱码原因是这个:',s)
-
         
 if __name__ == "__main__":
     print('ssln . i am just a spider object.')
-
